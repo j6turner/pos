e3a546d9f51cc388f29bc2434f95261bf616a8f3
Mike Harris & J. TURNER <student@epicodus.com>
2015-01-27 17:09:54 -0800

    Fix bugs in index and app.rb

--- app.rb
+++ app.rb
@@ -5,3 +5,23 @@ require("sinatra/activerecord")
 require("./lib/product")
 require("./lib/purchase")
 require("pg")
+
+get("/") do
+  @product = Product.all()
+  @purchase = Purchase.all()
+  erb(:index)
+end
+
+post("/purchase_post") do
+  purchase_date = params.fetch("purchase_date")
+  Purchase.new({:date => purchase_date}).save()
+  redirect("/")
+end
+
+post("/product_post") do
+  product_name = params.fetch("product_name")
+  product_price = params.fetch("product_price")
+  product_purchase_id = params.fetch("purchase_id")
+  Product.new({:name => product_name, :price => product_price, :purchase_id => product_purchase_id}).save()
+  redirect("/")
+end
--- lib/purchase.rb
+++ lib/purchase.rb
@@ -1,2 +1,3 @@
 class Purchase < ActiveRecord::Base
+  has_many(:products)
 end
